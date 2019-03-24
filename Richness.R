11533d07b91c62170c1bfe34b5081e81e0695d34
pwaryszak <dorpatczyk@yahoo.com>
2019-03-24 22:12:31 +1100

    Better Richness Table of GLMER output + Shannon-Wiener index as a response as well (table).

--- Richness.R
+++ Richness.R
@@ -5,7 +5,7 @@ library(Rmisc)
 data<- read.csv("TopsoilEmergenceData.csv")#our density data
 dim(data)#38247 obs. of  24 variables:
 
-# Run Test on Spring 2012 emergence=========
+#Spring 2012 Richness=========
 #Subset native plant species data for TST=0.5 (Spring 2012):
 df05 <- data %>% filter(TST==0.5 & nat=="native" ) %>% 
   select("su", "specCode", "count4m2", "plot2", "Transdepth", "rip", "fence") %>%
@@ -21,7 +21,7 @@ veg.wide05$DIVsimpson<-diversity(veg.wide05[,6:155], index= "simpson")
 veg.wide05$Density<-rowSums(veg.wide05[,6:155])
 veg.wide05$Richness<-specnumber(veg.wide05[,6:155])
 
-#Test for Spring 2013=========
+#Spring 2013 Richness=========
 #Subset native, perennials species data for TST=1.5 (Spring 2013):
 df1.5 <- data %>% filter(TST==1.5 & nat=="native") %>% 
   select("su", "specCode", "count4m2", "plot2", "Transdepth", "rip", "fence")%>%
@@ -36,6 +36,66 @@ veg.wide1.5$DIVshannon<-diversity(veg.wide1.5[,6:126])
 veg.wide1.5$DIVsimpson<-diversity(veg.wide1.5[,6:126], index= "simpson")
 veg.wide1.5$Density<-rowSums(veg.wide1.5[,6:126])
 veg.wide1.5$Richness<-specnumber(veg.wide1.5[,6:126])
+
+#Run Two Models (Spring 2012 and 2013):===========
+library(lme4)
+library(lmerTest)
+veg.wide05 $ su <- as.character(veg.wide05 $ su)
+veg.wide05$rip <- factor( veg.wide05$rip, levels = c("unripped","ripped"))#changing levels to show Coef-s in relation to ripped effect
+
+veg.wide05.site<-veg.wide05 %>%
+    separate(col = "su",into=c("site","cluster","plot"), sep="\\.", remove = F) %>%
+  filter(plot2 == "smoke" | plot2 == "heat" | plot2 == "herbicide" | plot2== "control")
+
+#Spring 2012 Model:
+glmer.spr12<-glmer(Richness ~ rip+fence+Transdepth+rip*fence+fence*Transdepth+
+                     rip*Transdepth + plot2 + rip*plot2 +Transdepth*plot2+
+                     (1|site/cluster),family = poisson(link="log"),
+                   data = veg.wide05.site)
+
+#Spring 2013 Model:
+veg.wide1.5 $ su <- as.character(veg.wide1.5 $ su)
+veg.wide1.5$rip <- factor( veg.wide1.5$rip, levels = c("unripped","ripped"))#changing levels to show Coef-s in relation to ripped effect
+
+veg.wide1.5.site<-veg.wide1.5 %>%
+  separate(col = "su",into=c("site","cluster","plot"), sep="\\.", remove = F) %>%
+  filter(plot2 == "smoke" | plot2 == "heat" | plot2 == "herbicide" | plot2== "control")
+
+glmer.spr13<-glmer(Richness ~ rip+fence+Transdepth+rip*fence+fence*Transdepth+
+                     rip*Transdepth + plot2 + rip*plot2 +Transdepth*plot2+
+                     (1|site/cluster),family = poisson(link="log"),
+                   data = veg.wide1.5.site)
+
+#Create Summary Table of that model:
+library("sjPlot")
+library("TMB")
+tab_model (glmer.spr12,glmer.spr13,
+           show.intercept = T, show.se = T, show.est = T,
+           transform = NULL, show.fstat = T, show.stat = T,
+           auto.label = FALSE, show.ci = FALSE,
+           title = "Richness as Response Variable (Two Models for Spring 2012 and 2013)")
+
+#Resources: https://strengejacke.github.io/sjPlot/articles/tab_model_estimates.html
+#http://127.0.0.1:22024/library/sjPlot/doc/tab_mixed.R
+
+#Spring SW-index 2012 Model:
+glmer.spr12_SW<-glmer(DIVshannon ~ rip+fence+Transdepth+rip*fence+fence*Transdepth+
+                     rip*Transdepth + plot2 + rip*plot2 +Transdepth*plot2+
+                     (1|site/cluster),family = poisson(link="log"),
+                   data = veg.wide05.site)
+
+#Spring SW-index 2013 Model:
+glmer.spr13_SW<-glmer(DIVshannon ~ rip+fence+Transdepth+rip*fence+fence*Transdepth+
+                     rip*Transdepth + plot2 + rip*plot2 +Transdepth*plot2+
+                     (1|site/cluster),family = poisson(link="log"),
+                   data = veg.wide1.5.site)
+
+tab_model (glmer.spr12_SW,glmer.spr13_SW,
+           show.intercept = T, show.se = T, show.est = T,
+           transform = NULL, show.fstat = T, show.stat = T,
+           auto.label = FALSE, show.ci = FALSE,
+           title = "Shannon-Wiener Diversity Index as Response Variable (Two Models for Spring 2012 and 2013)")
+
 
 #Richness SummarySE for spring2012 emergence:=====
 veg.wide05$season <- "Spring 2012"
@@ -92,42 +152,3 @@ Fig_Richness <-
 Fig_Richness
 #ggsave(Fig_Richness, filename = "Fig1_SiteScale_emergenceRichness.jpg", width = 180, height = 120, units = "mm")
 
-#Two Models (Spring 2012 and 2013):===========
-library(lme4)
-library(lmerTest)
-veg.wide05 $ su <- as.character(veg.wide05 $ su)
-veg.wide05$rip <- factor( veg.wide05$rip, levels = c("unripped","ripped"))#changing levels to show Coef-s in relation to ripped effect
-
-veg.wide05.site<-veg.wide05 %>%
-    separate(col = "su",into=c("site","cluster","plot"), sep="\\.", remove = F) %>%
-  filter(plot2 == "smoke" | plot2 == "heat" | plot2 == "herbicide" | plot2== "control")
-
-#Spring 2012 Model:
-glmer.spr12<-glmer(Richness ~ rip+fence+Transdepth+rip*fence+fence*Transdepth+
-                     rip*Transdepth + plot2 + rip*plot2 +Transdepth*plot2+
-                     (1|site/cluster),family = poisson(link="log"),
-                   data = veg.wide05.site)
-
-#Spring 2013 Model:
-veg.wide1.5 $ su <- as.character(veg.wide1.5 $ su)
-veg.wide1.5$rip <- factor( veg.wide1.5$rip, levels = c("unripped","ripped"))#changing levels to show Coef-s in relation to ripped effect
-
-veg.wide1.5.site<-veg.wide1.5 %>%
-  separate(col = "su",into=c("site","cluster","plot"), sep="\\.", remove = F) %>%
-  filter(plot2 == "smoke" | plot2 == "heat" | plot2 == "herbicide" | plot2== "control")
-
-glmer.spr13<-glmer(Richness ~ rip+fence+Transdepth+rip*fence+fence*Transdepth+
-                     rip*Transdepth + plot2 + rip*plot2 +Transdepth*plot2+
-                     (1|site/cluster),family = poisson(link="log"),
-                   data = veg.wide1.5.site)
-
-#Create Summary Table of that model:
-library("sjPlot")
-library("TMB")
-tab_model (glmer.spr12,glmer.spr13,
-           show.intercept = F, transform = NULL,
-           auto.label = FALSE, show.ci = FALSE,
-           title = "Richness as predictor (Two Models)")
-
-#Resources: https://strengejacke.github.io/sjPlot/articles/tab_model_estimates.html
-#http://127.0.0.1:22024/library/sjPlot/doc/tab_mixed.R
